d9988b0a31d4e37765827a553bffcf3a6f86879b
Luis-Urias <clerigodps@gmail.com>
2024-08-15 20:14:12 -0700

    Add files via upload

--- App.java
+++ App.java
@@ -2,10 +2,13 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 
 public class App {
     public static void main(String[] args) throws Exception {
+        System.out.println(Arrays.toString(args));
+        System.out.println(args.length);
         try {
             FileReader archivoFisico = new FileReader("C:\\Users\\Luis Urias\\codigospostales\\codigos_postales.csv");
 
@@ -28,6 +31,19 @@ public class App {
             }
             System.out.println("Numero de asentamientos Urbanos: "+counter);
             System.out.println("Numero de asentamientos Rurales: "+uwu);
+
+            while ( (registro = archivoLogico.readLine()) != null ) {
+                campos = registro.split(",");
+                int i = args.length;
+                    
+                if (campos[0].equals(args[i])) {
+                    System.out.println(Arrays.toString(campos));
+                }else if (campos[0].equals(owo)) {
+
+                }
+            }
+
+            archivoLogico.close();
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
